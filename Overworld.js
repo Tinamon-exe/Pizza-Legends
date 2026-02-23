d7d416542907ea7fea1429d7f44530a27b565185
Tinamon <tinamon.exe@outlook.com>
2026-02-23 17:00:27 +0100

    Video 2: Created GameObject class so that multiple objects/characters can be included.

--- Overworld.js
+++ Overworld.js
@@ -17,44 +17,67 @@ class Overworld {
    image.src = "/images/maps/DemoLower.png";
 
    //Get x and y coorinates for the hero and the shadow
-   const x = 5;
-   const y = 6;
+  //  const x = 5;
+  //  const y = 6;
   
-  // Get shadow image (need to be between overworld and hero)
-   const shadow = new Image();
-   shadow.onload = () => {
-    this.ctx.drawImage(
-      shadow, 
-      0, //left cut 
-      0, //top cut,
-      32, //width of cut
-      32, //height of cut
-      x * 16 - 8,
-      y * 16 - 18,
-      32,
-      32
-   )
-   }
-   shadow.src = "/images/characters/shadow.png";
+  // // Get shadow image (need to be between overworld and hero)
+  //  const shadow = new Image();
+  //  shadow.onload = () => {
+  //   this.ctx.drawImage(
+  //     shadow, 
+  //     0, //left cut 
+  //     0, //top cut,
+  //     32, //width of cut
+  //     32, //height of cut
+  //     x * 16 - 8,
+  //     y * 16 - 18,
+  //     32,
+  //     32
+  //  )
+  //  }
+  //  shadow.src = "/images/characters/shadow.png";
 
-  // Get hero image
-   const hero = new Image();
-   hero.onload = () => {
-     this.ctx.drawImage(
-       hero, 
-       0, //left cut 
-       0, //top cut,
-       32, //width of cut
-       32, //height of cut
-       x * 16 - 8,  // * 16 since ever tile is 16x16,  -8 since the sprite may be 32X32 but the art is a little bit smaller
-       y * 16 - 18, // * 16 since ever tile is 16x16, -18 since the sprite may be 32X32 but the art is a little bit smaller
-       32, //size/scale of cut x
-       32  // size/scale of cut y
-    )
-   }
-   hero.src = "/images/characters/people/hero.png";
+  // // Get hero image
+  //  const hero = new Image();
+  //  hero.onload = () => {
+  //    this.ctx.drawImage(
+  //      hero, 
+  //      0, //left cut 
+  //      0, //top cut,
+  //      32, //width of cut
+  //      32, //height of cut
+  //      x * 16 - 8,  // * 16 since ever tile is 16x16,  -8 since the sprite may be 32X32 but the art is a little bit smaller
+  //      y * 16 - 18, // * 16 since ever tile is 16x16, -18 since the sprite may be 32X32 but the art is a little bit smaller
+  //      32, //size/scale of cut x
+  //      32  // size/scale of cut y
+  //   )
+  //  }
+  //  hero.src = "/images/characters/people/hero.png";
 
 
+
+
+
+  // Place some Game Objects
+  const hero = new GameObject({
+    //Get x and y coorinates for the hero and the shadow
+    x: 5,
+    y: 6,
+
+  })
+
+  const npc1 = new GameObject({
+    x: 7,
+    y: 9,
+    src: "/images/characters/people/npc1.png"
+    
+  })
+
+  setTimeout(() => {
+    hero.sprite.draw(this.ctx)
+    npc1.sprite.draw(this.ctx) 
+  }, 200)
+  
  }
 
 }
---
+++ Sprite.js
@@ -0,0 +1,60 @@
+class Sprite {
+    constructor(config) {
+        //Set up the image
+        this.image = new Image();
+        this.image.src = config.src;
+        this.image.onload = () => {
+            this.isLoaded = true;
+        };
+        
+        // Shadow
+        this.shadow = new Image();
+        this.useShadow=true;
+        if (this.useShadow){
+            this.shadow.src = "/images/characters/shadow.png";
+        }
+        this.shadow.onload = () => {
+            this.isShadowLoaded = true;
+        }
+        
+
+        // Configuring animation & initial state
+        this.animations=config.animations ||{
+            idleDown: [
+                [0,0]
+            ]
+        }
+        this.currentAnimation = config.currentAnimation || "idleDown";
+        this.currentAnimationFrame =0;
+
+        // Reference the game object
+        this.gameObject = config.gameObject;
+    }
+
+    draw(ctx) {
+        const x = this.gameObject.x*16-8; // * 16 since ever tile is 16x16,  -8 since the sprite may be 32X32 but the art is a little bit smaller
+        const y = this.gameObject.y*16-18; // * 16 since ever tile is 16x16, -18 since the sprite may be 32X32 but the art is a little bit smaller
+
+        // Draw Shadow before character
+        this.isShadowLoaded && ctx.drawImage(
+            this.shadow, 
+            x,  
+            y,
+        )
+
+
+        // Make sure it is loaded before drawing
+        this.isLoaded && ctx.drawImage(
+            this.image, 
+            0, //left cut 
+            0, //top cut,
+            32, //width of cut
+            32, //height of cut
+            x,  
+            y , 
+            32, //size/scale of cut x
+            32  // size/scale of cut y
+        )
+
+    }
+}
